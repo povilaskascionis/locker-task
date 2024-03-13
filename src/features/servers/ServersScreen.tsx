7bb138aa2141d124f0ccbb3970a250b24eb42487
Povilas Kascionis <povilas.kascionis@podimo.com>
2024-03-13 09:17:13 +0200

    table styling

--- src/features/servers/ServersScreen.tsx
+++ src/features/servers/ServersScreen.tsx
@@ -3,11 +3,13 @@ import {
   getCoreRowModel,
   useReactTable,
   flexRender,
+  getSortedRowModel,
 } from '@tanstack/react-table'
 import { useGetServersQuery } from '../api/apiSlice'
 import { Server } from '../api/types'
 import Layout from '@/components/layout/Layout'
 import Spinner from '@/components/spinner/Spinner'
+import styles from './ServersScree.module.scss'
 
 const colHelper = createColumnHelper<Server>()
 
@@ -15,10 +17,12 @@ const columns = [
   colHelper.accessor('name', {
     header: () => 'Name',
     cell: (props) => props.getValue(),
+    sortingFn: 'alphanumeric',
   }),
   colHelper.accessor('distance', {
     header: () => 'Distance',
     cell: (props) => props.getValue(),
+    sortingFn: 'alphanumeric',
   }),
 ]
 
@@ -29,6 +33,7 @@ const ServersScreen = () => {
     data: servers.data ?? [],
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   })
   console.log(servers.isLoading)
   return (
@@ -36,13 +41,13 @@ const ServersScreen = () => {
       {servers.isLoading && <Spinner />}
       <Layout>
         {!servers.isLoading && (
-          <table>
-            <thead>
+          <table className={styles.table}>
+            <thead className={styles.tableHead}>
               {table.getHeaderGroups().map((headerGroup) => (
                 <tr key={headerGroup.id}>
                   {headerGroup.headers.map((header) => (
                     <th key={header.id}>
-                      <div>
+                      <div className={styles.headerCell}>
                         {flexRender(
                           header.column.columnDef.header,
                           header.getContext(),
@@ -57,7 +62,7 @@ const ServersScreen = () => {
               {table.getRowModel().rows.map((row) => (
                 <tr key={row.id}>
                   {row.getVisibleCells().map((cell) => (
-                    <td key={cell.id}>
+                    <td key={cell.id} className={styles.tableCell}>
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext(),
